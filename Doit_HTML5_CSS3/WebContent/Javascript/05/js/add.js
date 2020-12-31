0acd808af49133b511fb4f26d2e9885fc330a35a
NyeongB <kdtoaj14@ajou.ac.kr>
2020-12-31 16:57:54 +0900

    addNumber 반환형

--- Doit_HTML5_CSS3/WebContent/Javascript/05/js/add.js
+++ Doit_HTML5_CSS3/WebContent/Javascript/05/js/add.js
@@ -2,7 +2,14 @@
  * 
  */
 
-function addNumber()
+var num1 = parseInt(prompt('첫 번째 숫자?'));
+var num2 = parseInt(prompt('두 번째 숫자?'));
+var result = addNumber(num1, num2)
+alert('두 수를 더한 값은 ' + result + '입니다');
+function addNumber(a, b)
 {
-	alert(10 + 20);
+	
+	var sum = a + b;
+	return sum;
+	
 }
